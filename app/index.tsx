4a05fcd8ed7875d85f974de8bb890851b0fb90f2
aashi129 <aashi.patel108@gmail.com>
2024-06-07 17:18:23 -0400

    Added map and pins

--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,59 @@
-// import { Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Text>Edit app/index.tsx to edit this screen.</Text>
-//     </View>
-//   );
-// }
-
-import React, { useState, useEffect } from 'react';
-import { Text, View, FlatList, StyleSheet } from 'react-native';
-
-export default function Index() {
-  const [items, setItems] = useState<any[]>([]);
-
-  useEffect(() => {
-    fetch('http://172.20.10.3:8000/test.php')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Data from server:', data);
-        setItems(data);
-      })
-      .catch(error => console.error(error));
-  }, []);
-
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Items</Text> 
-      <FlatList
-        data={items}
-        renderItem={({ item }) => <Text style={styles.item}>{item.uid}</Text>} // Access uid property instead of name
-        keyExtractor={item => item.uuid} // Use uid instead of id
-      />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 0,
+          longitude: 0,
+          latitudeDelta: 2,
+          longitudeDelta: 2,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 43,
+            longitude: -80.4960872580559,
+          }}
+          pinColor="red"
+        >
+          <Callout>
+            <View style={styles.callout}>
+              <Text style={styles.calloutTitle}>Your Marker Title</Text>
+              <Text style={styles.calloutDescription}>Your Marker Description</Text>
+              <Text style={styles.calloutDescription}>Your Marker Description</Text>
+              <Text style={styles.calloutDescription}>Your Marker Description</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22,
-    paddingHorizontal: 10,
   },
-  title: {
-    fontSize: 24,
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  callout: {
+    width: 150,
+    padding: 5,
+  },
+  calloutTitle: {
     fontWeight: 'bold',
-    marginBottom: 10,
+    fontSize: 16,
   },
-  item: {
-    fontSize: 18,
-    marginBottom: 5,
+  calloutDescription: {
+    fontSize: 14,
   },
 });
+
+export default App;
