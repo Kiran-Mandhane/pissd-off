4b5cb90cad8e056b356a7ead559925a68f9b7d5e
Kiran Mandhane <k.mandhane@gmail.com>
2024-06-20 16:49:55 -0400

    sql queries for submitting a form

--- test.php
+++ test.php
@@ -43,7 +43,7 @@ else if($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/t
    // Assuming $data now holds the JSON-decoded body
    if ($data) {
        // Extract data from the received JSON
-       $reviewTimestamp = $data['reviewTimestamp'];
+       $formTimestamp = $data['reviewTimestamp'];
        $gender = $data['gender'];
        $cleanliness = $data['cleanliness'];
        $waitTime = $data['waitTime'];
@@ -51,11 +51,26 @@ else if($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/t
 
        // Print the received data
        echo "Received rating data:\n";
-       echo "Review Timestamp: $reviewTimestamp\n";
+       echo "Form Timestamp: $formTimestamp\n";
        echo "Gender: $gender\n";
        echo "Cleanliness: $cleanliness\n";
        echo "Wait Time: $waitTime\n";
 
+       $date = date('Y-m-d H:i:s'); // Convert string to date format.
+       
+
+       $getMaxFormId = "SELECT formId + 1 FROM Forms WHERE formId >= ALL(SELECT formId FROM Forms);";
+       $formId = $conn->query($getMaxFormId);
+       $intFormId = $formId->fetch_array()[0] ?? NULL;
+
+
+    $insertForms = "INSERT INTO Forms (formId, waitTime, cleanliness, gender, formTimestamp)
+                VALUES ($intFormId, $waitTime, $cleanliness, '{$gender}', '{$date}');";
+        if ($conn->query($insertForms) === TRUE) {
+            echo "New record inserted successfully";
+        } else {
+            echo "Error: " . $insertForms . "<br>" . $conn->error;
+        }
 
        // Optionally, you can send a response back to your React Native app
        $response = array('message' => 'Rating data received successfully');
