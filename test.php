02b35fb9dce0ab0ad537c03093e4af196b3994b5
Kiran Mandhane <k.mandhane@gmail.com>
2024-06-20 23:40:03 -0400

    add insertFormOf

--- test.php
+++ test.php
@@ -48,7 +48,6 @@ else if($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/t
        $cleanliness = $data['cleanliness'];
        $waitTime = $data['waitTime'];
 
-
        // Print the received data
        echo "Received rating data:\n";
        echo "Form Timestamp: $formTimestamp\n";
@@ -56,21 +55,26 @@ else if($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/t
        echo "Cleanliness: $cleanliness\n";
        echo "Wait Time: $waitTime\n";
 
-       $date = date('Y-m-d H:i:s'); // Convert string to date format.
+        $date = date('Y-m-d H:i:s'); // Convert string to date format.
        
 
-       $getMaxFormId = "SELECT formId + 1 FROM Forms WHERE formId >= ALL(SELECT formId FROM Forms);";
-       $formId = $conn->query($getMaxFormId);
-       $intFormId = $formId->fetch_array()[0] ?? NULL;
-
-
-    $insertForms = "INSERT INTO Forms (formId, waitTime, cleanliness, gender, formTimestamp)
+        $getMaxFormId = "SELECT formId + 1 FROM Forms WHERE formId >= ALL(SELECT formId FROM Forms);";
+        $formId = $conn->query($getMaxFormId);
+        $intFormId = $formId->fetch_array()[0] ?? NULL;
+        $insertForms = "INSERT INTO Forms (formId, waitTime, cleanliness, gender, formTimestamp)
                 VALUES ($intFormId, $waitTime, $cleanliness, '{$gender}', '{$date}');";
         if ($conn->query($insertForms) === TRUE) {
             echo "New record inserted successfully";
         } else {
             echo "Error: " . $insertForms . "<br>" . $conn->error;
         }
+
+        $insertisFormOf = "INSERT INTO isFormOf (washroomId, formId) VALUES (1, $intFormId);";
+            if ($conn->query($insertisFormOf) === TRUE) {
+                echo "New record inserted successfully";
+            } else {
+                echo "Error: " . $insertisFormOf . "<br>" . $conn->error;
+            }
 
        // Optionally, you can send a response back to your React Native app
        $response = array('message' => 'Rating data received successfully');
